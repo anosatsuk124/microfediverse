6e90b3b9d00a08955a5a241222cdca5f99822f01
Satsuki Akiba <anosatsuk124@gmail.com>
2022-07-17 22:26:05 +0900

    fix

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,8 @@
 <script lang="ts">
-  import svelteLogo from "./assets/svelte.svg";
-  import Counter from "./lib/Counter.svelte";
 </script>
 
 <main>
   <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
 
   <p>
     Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank"
@@ -20,17 +14,6 @@
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
   .read-the-docs {
     color: #888;
   }
