ac855a0742b1a1d016a81d94e8dae09addd787d1
Satsuki Akiba <anosatsuk124@gmail.com>
2022-07-20 17:38:28 +0900

    create from entity files

---
+++ src-tauri/src/database/entity/users.rs
@@ -0,0 +1,25 @@
+use sea_orm::entity::prelude::*;
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel)]
+#[sea_orm(table_name = "users")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub id: i32,
+    pub name: String,
+    #[sea_orm(column_type = "Text", nullable)]
+    pub description: Option<String>,
+    pub deleted: bool,
+    pub created_at: DateTimeWithTimeZone,
+    pub updated_at: DateTimeWithTimeZone,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter)]
+pub enum Relation {}
+
+impl RelationTrait for Relation {
+    fn def(&self) -> RelationDef {
+        panic!("No RelationDef")
+    }
+}
+
+impl ActiveModelBehavior for ActiveModel {}
--- src-tauri/src/database/mod.rs
+++ src-tauri/src/database/mod.rs
@@ -1,22 +1,34 @@
-mod migrator;
-
-use crate::database::migrator::Migrator;
-use sea_orm::{Database, DbErr};
+mod entity;
+use sea_orm::{ConnectionTrait, Database, DbErr, Schema};
 use sea_orm_migration::prelude::*;
-
-const DATABASE_URL: &str = if cfg!(debug_assertions) {
-    "sqlite::memory:"
-} else {
-    "sqlite://data.db"
-};
+use std::fs::File;
+use std::path;
 
 async fn run() -> Result<(), DbErr> {
-    let db = Database::connect(DATABASE_URL).await?;
+    let database_path = "data.sqlite3";
+
+    let database_url = if cfg!(debug_assertions) {
+        "sqlite::memory:".to_owned()
+    } else {
+        format!("sqlite://{}", database_path)
+    };
+
+    let db = if !cfg!(debug_assertions) && !path::Path::new(&database_path).exists() {
+        File::create(&database_path).unwrap();
+        Database::connect(database_url).await?
+    } else {
+        Database::connect(database_url).await?
+    };
+
+    let backend = db.get_database_backend();
+    let schema = Schema::new(backend);
+
+    let st = backend.build(&schema.create_table_from_entity(entity::users::Entity));
+    db.execute(st).await?;
 
     let schema_manager = SchemaManager::new(&db);
 
-    Migrator::refresh(&db).await?;
-    println!("{:?}", schema_manager.has_table("bakery").await?);
+    println!("{:?}", schema_manager.has_table("users").await?);
 
     Ok(())
 }
